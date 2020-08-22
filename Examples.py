9241c82f01cf394fb95c20f3b71707368094fef7
Toshukojo <Toshukojo@users.noreply.github.com>
2020-08-21 21:14:51 -0400

    Examples

--- Examples.py
+++ Examples.py
@@ -1,5 +1,20 @@
-s = 0
+m = list()
 for n in range(1, 10):
-    if n % 3 == 0 or n % 5 ==0:
-        s += n
-print(s)
+    if n % 3 == 0 or n % 5 == 0:
+        m.append(n)
+print(sum(m))
+
+
+class Summable_List(list):
+    def sum(self):
+        s = 0
+        for v in self:
+            s += v
+        return s
+
+
+# The sum of a sequence has a simple, recursive definition
+def sumr(seq):
+    if len(seq) == 0: return 0
+    return seq[0] + sumr(seq[1:])
+
