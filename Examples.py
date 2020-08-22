417f3ca957ac4455a22672034cfbceaf1752b7f2
Toshukojo <Toshukojo@users.noreply.github.com>
2020-08-21 23:35:22 -0400

    examples

--- Examples.py
+++ Examples.py
@@ -18,3 +18,15 @@ def sumr(seq):
     if len(seq) == 0: return 0
     return seq[0] + sumr(seq[1:])
 
+
+def until(n, filter_func, v):
+    if v == n: return []
+    if filter_func(v):
+        return [v] + until(n, filter_func, v + 1)
+    else:
+        return until(n, filter_func, v + 1)
+
+
+
+
+mult_3_5 = lambda x: x % 3 == 0 or x % 5 == 0
