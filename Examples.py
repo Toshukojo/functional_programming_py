dbec815c62419943f9d04d8dc1ea4c6117bba040
Toshukojo <Toshukojo@users.noreply.github.com>
2020-08-24 13:14:42 -0400

    Functional Programming Example Code

--- Examples.py
+++ Examples.py
@@ -40,8 +40,22 @@ def repeat(f, a):
         yield v
 
 def within(ε, iterable):
- def head_tail(ε, a, iterable):
- b = next(iterable)
- if abs(a-b) <= ε: return b
- return head_tail(ε, b, iterable)
- return head_tail(ε, next(iterable), iterable)
+    def head_tail(ε, a, iterable):
+        b = next(iterable)
+        if abs(a-b) <= ε: return b
+            return head_tail(ε, b, iterable)
+    return head_tail(ε, next(iterable), iterable)
+
+def sqrt(a0, ε, n):
+    return within(ε, repeat(lambda x: next_(n, x), a0))
+
+def example(a, b, **kw):
+    return a*b
+
+mersenne = lambda x: 2**x-1
+
+year_cheese = [(2000, 29.87), (2001, 30.12), (2002, 30.6), (2003,
+30.66),(2004, 31.33), (2005, 32.62), (2006, 32.73), (2007, 33.5),
+(2008, 32.84), (2009, 33.02), (2010, 32.92)]
+
+max(year_cheese)
